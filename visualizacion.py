5a39de0b2b7865ad94827dee637500bf1d873867
kevinospi24 <kevinospinag2403@gmail.com>
2025-05-14 15:10:53 -0500

    Añadir los primeros archivos

---
+++ visualizacion.py
@@ -0,0 +1,16 @@
+# Librerías necesarias
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# Seaborn para visualizaciones más estilizadas
+import seaborn as sns
+import os
+from analisis import DataAnalyzer
+
+data = pd.read_csv("adult.csv")
+
+analizar = DataAnalyzer(data)
+
+analizar.summary()
+analizar.correlation_matrix()
+analizar.categorical_analisis()
